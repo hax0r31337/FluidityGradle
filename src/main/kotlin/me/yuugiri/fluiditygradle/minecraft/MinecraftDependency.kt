a4ba01fa3926b77cd584ad5088d9559d5161e518
hax0r31337 <liulihaocaiqwq@gmail.com>
2022-10-10 21:52:40 +0800

    fix minecraft deobfuscation

--- src/main/kotlin/me/yuugiri/fluiditygradle/minecraft/MinecraftDependency.kt
+++ src/main/kotlin/me/yuugiri/fluiditygradle/minecraft/MinecraftDependency.kt
@@ -1,4 +1,3 @@
-
 import me.yuugiri.fluiditygradle.minecraft.MinecraftVersion
 import me.yuugiri.fluiditygradle.remap.ErroringRemappingAccessMap
 import me.yuugiri.fluiditygradle.utils.cacheDir
@@ -9,10 +8,13 @@ import net.md_5.specialsource.Jar
 import net.md_5.specialsource.JarMapping
 import net.md_5.specialsource.JarRemapper
 import net.md_5.specialsource.RemapperProcessor
+import net.md_5.specialsource.provider.JarProvider
+import net.md_5.specialsource.provider.JointProvider
 import org.gradle.api.Project
 import org.gradle.api.file.ConfigurableFileCollection
 import java.io.BufferedReader
 import java.io.File
+
 
 fun minecraftDep(project: Project, vararg accessTransformer: String): String {
     val minecraftJar = minecraftJar(project)
@@ -23,10 +25,17 @@ fun minecraftDep(project: Project, vararg accessTransformer: String): String {
 
         val accessMap = ErroringRemappingAccessMap() // TODO: consider use original AccessMap when csv support is unused
         accessTransformer.forEach { accessMap.loadAccessTransformer(File(project.rootDir, it)) }
+        val input = Jar.init(version.getJars(cacheDir))
         val mapping = JarMapping()
         mapping.loadMappings(BufferedReader(srgMapping.reader(Charsets.UTF_8)), null, null, false)
+
+        // ensure that inheritance provider is used
+        val inheritanceProviders = JointProvider()
+        inheritanceProviders.add(JarProvider(input))
+        mapping.setFallbackInheritanceProvider(inheritanceProviders)
+
         val remapper = JarRemapper(RemapperProcessor(null, mapping, null), mapping, RemapperProcessor(null, null, accessMap))
-        remapper.remapJar(Jar.init(version.getJars(cacheDir)), minecraftJar)
+        remapper.remapJar(input, minecraftJar)
 
         // check for broken accessMap
         if (accessMap.brokenLines.isNotEmpty()) {
